696efe1aef942e887bbf20b59ebc2b27c3a6adf4
Sarsfield Zelda <sazelda@at-j1.msk.21-school.ru>
2021-10-25 19:38:25 +0300

    strange

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sazelda <sazelda@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 15:06:31 by sazelda           #+#    #+#             */
+/*   Updated: 2021/10/25 19:37:33 by sazelda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <unistd.h>
 #include "get_next_line.h"
@@ -7,80 +19,68 @@
 char	*get_next_line(int fd)
 {
 	char	buf[BUFFER_SIZE + 1];
-	char	*p_n;
+	char	*p_n = NULL;
 	static char 	*ost;
-	char *res;
+	char *res = NULL;
+	char *tmp;
 	int n_read;
-	char	*tmp;
-
-	p_n = NULL;
 	
-	if (!ost)
+	//printf("ost = %s", ost);
+	while (!p_n && !res)
 	{
-		res = malloc(1);
-		res[0] = '\0';
-	}
-	else
-	{
-		p_n = ft_strchr(ost, '\n');
-		if (p_n)
-		{
-			*p_n = '\0';
-			res = ft_strdup(ost);
-			p_n++;
-			ost = p_n;
-		}
-		else
-		{
-			res = ft_strdup(ost);
-			ost = NULL;
-		}
-	}
-
-	n_read = read(fd, &buf, BUFFER_SIZE);
-	if (n_read < 0)
-	{
-		if (res)
-			free(res);
 		if (ost)
-			free(ost);
-		return (NULL);
-	}
-	buf[n_read] = '\0';
-
-	while (!p_n && n_read)
-	{
-		p_n = ft_strchr(buf, '\n');
+			p_n = ft_strchr(ost, '\n');
+		//printf("ost = %s\n", ost);
 		if (p_n)
 		{
 			*p_n = '\0';
-			ost = ft_strdup(++p_n);
-			tmp = res;
-			res = ft_strjoin(res, buf);
-			if (tmp)
-				free(tmp);
-		}
-		else
-		{
-			tmp = res;
-			res = ft_strjoin(res, buf);
-			free(tmp);
-			n_read = read(fd, &buf, BUFFER_SIZE);
-			if (n_read < 0)
+			res = ost;
+
+			ost = ++p_n;
+			if (ost[0] == '\0')
 			{
-				if (res)
-					free(res);
-				if (ost)
-					free(ost);
+				ost = NULL;
 				return (NULL);
 			}
-			buf[n_read] = '\0';
+			if (res[0] == '\0')
+				return ("\n");
+			return (res);
+		}
+		else
+		{
+			n_read = read(fd, &buf, BUFFER_SIZE);
+			if (n_read > 0)
+			{
+				buf[n_read] = '\0';
+				//printf("BUF=%s\n", buf);
+				//printf("%s", ost);
+				if (!ost)
+				{
+					ost = malloc(2);
+					ost[0] = '\0';
+				}
+				tmp = ost;
+				ost = ft_strjoin(ost, buf);
+				free(tmp);
+			}
+			else if (n_read == 0)
+			{
+				ost = ft_strjoin(ost, "\n");
+			}
+			else if (n_read < 0)
+			{
+				return (NULL);
+			}
 		}
 	}
-	if (p_n)
-		tmp = res;
-		res = ft_strjoin(res, "\n");
-		if (tmp)
-			free(tmp);
+	//printf("LAST ost = %s", ost);
 	return (res);
 }
+
+// int main()
+// {
+// 	int fd;
+// 	fd = open("test.txt", O_RDONLY);
+// 	printf("res = %s\n", get_next_line(fd));
+// 	printf("res = %s\n", get_next_line(fd));
+// }
