7144d3734fe37c53c09eb326b095ddd62157bedb
Sarsfield Zelda <sazelda@at-n5.msk.21-school.ru>
2021-12-02 13:23:04 +0300

    only strange

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sazelda <sazelda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 15:06:31 by sazelda           #+#    #+#             */
-/*   Updated: 2021/10/25 19:37:33 by sazelda          ###   ########.fr       */
+/*   Updated: 2021/12/02 13:21:20 by sazelda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,43 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
+int	find_n(char *ost)
+{
+	int i;
+	i = 0;
+	while(ost[i] != '\0')
+	{
+		if (ost[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (-1);
+}
 
 static char	*ft_check_ost(char *ost, char **res, int n_read)
 {
-	char	*p_n;
-	p_n = ft_strchr(ost, '\n');
-	if (p_n)
+	//char	*p_n;
+	int id_n;
+	//char		*for_free;
+	int i;
+	id_n = find_n(ost);
+	//p_n = ft_strchr(ost, '\n');
+	if (id_n != -1)
 	{
-		*p_n = '\0';
-		*res = ft_strdup(ost);
-		*res = ft_strjoin(*res, "\n");
-		p_n++;
-		if (*p_n != '\0')
-			ost = p_n;
+		*res = (char *)malloc(id_n + 1);
+		i = 0;
+		while (i < id_n)
+		{
+			(*res)[i] = ost[i];
+			i++;
+		}
+		(*res)[i] = '\0';
+		if (ost[i] != '\0')
+			ost = &ost[i];
 		else
 			ost = NULL;
 	}
-	else if (!p_n && n_read == 0)
+	else if (id_n == -1 && n_read == 0)
 	{
 		*res = ft_strdup(ost);
 		ost = NULL;
@@ -45,6 +65,7 @@ char	*get_next_line(int fd)
 	char		buf[BUFFER_SIZE + 1];
 	char		*res;
 	static char	*ost;
+	char		*for_free;
 	int			n_read;
 
 	res = NULL;
@@ -55,9 +76,15 @@ char	*get_next_line(int fd)
 		if (n_read > 0)
 		{
 			if (ost)
+			{
+				for_free = ost;
 				ost = ft_strjoin(ost, buf);
+				free(for_free);
+			}
 			else
+			{
 				ost = ft_strdup(buf);
+			}
 		}
 		else if (n_read == 0)
 		{
@@ -65,15 +92,25 @@ char	*get_next_line(int fd)
 			{
 				ost = ft_check_ost(ost, &res, n_read);
 				if (res)
+				{
+					free(ost);
 					return (res);
+				}
 			}
 			else
+			{
+				free(ost);
+				free(res);
 				return (NULL);
+			}
 		}
 		else
+		{
 			return (NULL);
+		}
 		ost = ft_check_ost(ost, &res,n_read);
 	}
+	free(ost);
 	return (res);
 }
 
@@ -81,13 +118,5 @@ char	*get_next_line(int fd)
 // {
 // 	int fd;
 // 	fd = open("test.txt", O_RDONLY);
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
-// 	printf("res = %s", get_next_line(fd));
 // 	printf("res = %s", get_next_line(fd));
 // }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,15 @@
 /*   By: sazelda <sazelda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 15:06:21 by sazelda           #+#    #+#             */
-/*   Updated: 2021/10/25 15:06:23 by sazelda          ###   ########.fr       */
+/*   Updated: 2021/12/02 12:41:35 by sazelda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+# define BUFFER_SIZE 1
 # include <stdlib.h>
+
 
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
